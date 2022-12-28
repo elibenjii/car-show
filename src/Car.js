86d0891f128133a8102959de7564970656e1acf8
yolo <niels.dominguez@gmail.com>
2022-12-28 16:10:40 +0700

    clean

--- src/Car.js
+++ src/Car.js
@@ -1,37 +1,40 @@
-import React, { useEffect } from "react"
-import { useFrame, useLoader } from "@react-three/fiber"
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
+import React, { useEffect, Suspense } from "react"
+import { useFrame } from "@react-three/fiber"
+import { useGLTF } from "@react-three/drei"
 import { Mesh } from "three"
 
 // based on "Chevrolet Corvette (C7)" (https://sketchfab.com/3d-models/chevrolet-corvette-c7-2b509d1bce104224b147c81757f6f43a)
 // by Martin Trafas (https://sketchfab.com/Bexxie) licensed under CC-BY-4.0 (http://creativecommons.org/licenses/by/4.0/)
 const Car = () => {
-  const gltf = useLoader(GLTFLoader, "models/car/scene.gltf")
+  const { scene } = useGLTF("models/car/scene.gltf")
 
   useEffect(() => {
-    console.log("gltf", gltf)
-    gltf.scene.scale.set(0.005, 0.005, 0.005)
-    gltf.scene.position.set(0, -0.035, 0)
-    gltf.scene.traverse((object) => {
+    scene.scale.set(0.005, 0.005, 0.005)
+    scene.position.set(0, -0.035, 0)
+    scene.traverse((object) => {
       if (object instanceof Mesh) {
         object.castShadow = true
         object.receiveShadow = true
         object.material.envMapIntensity = 20
       }
     })
-  }, [gltf])
+  }, [scene])
 
   useFrame((state, delta) => {
     const t = state.clock.getElapsedTime() * 2
 
-    const { children } = gltf.scene.children[0].children[0].children[0]
+    const { children } = scene.children[0].children[0].children[0]
     children[0].rotation.x = t
     children[2].rotation.x = t
     children[4].rotation.x = t
     children[6].rotation.x = t
   })
 
-  return <primitive object={gltf.scene} />
+  return (
+    <Suspense fallback={null}>
+      <primitive object={scene} />
+    </Suspense>
+  )
 }
 
 export default Car
